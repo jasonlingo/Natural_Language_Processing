a5388e3528fc9893f1af8211cefdd68dea3eb4d0
Li-Yi Lin <jasonlingo@gmail.com>
2016-02-24 20:44:09 -0500

    commit

---
+++ HW3/hw-lm/run.sh
@@ -0,0 +1,23 @@
+printf 1k
+python python/textcat.py add2.7 lexicons/words-10.txt english_spanish/train/en.1k english_spanish/train/sp.1k english_spanish/test/english/*/*
+python python/textcat.py add2.7 lexicons/words-10.txt english_spanish/train/en.1k english_spanish/train/sp.1k english_spanish/test/spanish/*/*
+
+# printf 2k
+# python python/textcat.py backoff_add2.7 lexicons/words-10.txt english_spanish/train/en.2k english_spanish/train/sp.2k english_spanish/test/english/*/*
+# python python/textcat.py backoff_add2.7 lexicons/words-10.txt english_spanish/train/en.2k english_spanish/train/sp.2k english_spanish/test/spanish/*/*
+
+# printf 5k
+# python python/textcat.py backoff_add2.7 lexicons/words-10.txt english_spanish/train/en.5k english_spanish/train/sp.5k english_spanish/test/english/*/*
+# python python/textcat.py backoff_add2.7 lexicons/words-10.txt english_spanish/train/en.5k english_spanish/train/sp.5k english_spanish/test/spanish/*/*
+
+# printf 10k
+# python python/textcat.py backoff_add2.7 lexicons/words-10.txt english_spanish/train/en.10k english_spanish/train/sp.10k english_spanish/test/english/*/*
+# python python/textcat.py backoff_add2.7 lexicons/words-10.txt english_spanish/train/en.10k english_spanish/train/sp.10k english_spanish/test/spanish/*/*
+
+# printf 20k
+# python python/textcat.py backoff_add2.7 lexicons/words-10.txt english_spanish/train/en.20k english_spanish/train/sp.20k english_spanish/test/english/*/*
+# python python/textcat.py backoff_add2.7 lexicons/words-10.txt english_spanish/train/en.20k english_spanish/train/sp.20k english_spanish/test/spanish/*/*
+
+# printf 50k
+# python python/textcat.py backoff_add2.7 lexicons/words-10.txt english_spanish/train/en.50k english_spanish/train/sp.50k english_spanish/test/english/*/*
+# python python/textcat.py backoff_add2.7 lexicons/words-10.txt english_spanish/train/en.50k english_spanish/train/sp.50k english_spanish/test/spanish/*/*
