0428d645be5c3bffcf4122bdf81dbd948c97aa96
Li-Yi Lin <jasonlingo@gmail.com>
2016-03-13 22:47:58 -0400

    commit before merge

---
+++ HW4/src/main/java/Earley.java
@@ -0,0 +1,52 @@
+import java.util.HashMap;
+import Rule;
+
+/**
+ * Created by Jason on 3/13/16.
+ */
+public class Earley {
+
+    Map<String, List<Rule>> check;
+    Map<String, List<Rule>> rules;
+    Rule chartHead;
+    Rule chartTail;
+
+    public Earley() {
+        this.check = new HashMap<>();
+        this.chartHead = null;
+        this.chartTail = null;
+        this.rules = null;
+    }
+
+
+    public void addRules(Map<String, List<Rule>> rules) {
+        this.rules = rules;
+    }
+
+    /*
+     For each non-terminal 
+     */
+    private void predict() {
+
+    }
+
+    private void scan() {
+
+    }
+
+    private void attach() {
+
+    }
+
+    private void addToChart() {
+
+    }
+
+    /*
+     Parse input sentence using Earley algorithm.
+     */
+    public void parse(String sen) {
+
+    }
+
+}
---
+++ HW4/src/main/java/Parse.java
@@ -0,0 +1,11 @@
+/**
+ * Created by Jason on 3/13/16.
+ */
+public class Parse {
+
+    public static void main (String[] args) {
+        for (String s: args) {
+            System.out.println(s);
+        }
+    }
+}
